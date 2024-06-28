43f276186121950e668b927376a7de58baac55a9
yunsehwan <sh10302@naver.com>
2024-06-29 07:53:39 +0900

    [FEAT] #3 디자인 시스템 checkbox 구현

---
+++ core/ui/src/main/java/pokitmons/pokit/core/ui/components/atom/checkbox/attributes/CheckboxAttributes.kt
@@ -0,0 +1,9 @@
+package pokitmons.pokit.core.ui.components.atom.checkbox.attributes
+
+enum class PokitCheckboxStyle{
+    FILLED, STROKE, ICON_ONLY
+}
+
+enum class PokitCheckboxShape{
+    RECTANGLE, CIRCLE
+}
