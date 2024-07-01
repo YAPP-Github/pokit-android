c10ded7534a4d5dda4ed0736f0ac35cb90dc898c
jiwon2724 <stopone3119@naver.com>
2024-07-01 23:38:48 +0900

    [chore] 미사용 리소스 제거 및 즁요 정보 은닉화

--- feature/login/build.gradle.kts
+++ feature/login/build.gradle.kts
@@ -70,7 +70,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.googleid)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
