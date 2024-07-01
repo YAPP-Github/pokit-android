5021847cd34432ef2026b987955639ae65562eb4
jiwon2724 <stopone3119@naver.com>
2024-07-01 18:18:39 +0900

    [chore] theme 디자인 시스템 반영 및 로그인 화면 등록

---
+++ feature/login/src/main/java/pokitmons/pokit/login/LoginScreen.kt
@@ -0,0 +1,9 @@
+package pokitmons.pokit.login
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoginScreen() {
+    Text(text = "로그인 테스트 화면")
+}
