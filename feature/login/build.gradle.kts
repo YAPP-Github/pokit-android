49d526d98544dba0d6118b6b794c4092c6ff0026
jiwon2724 <stopone3119@naver.com>
2024-07-02 00:06:11 +0900

    [chore] properties.getProperty("web_client_id") null인 경우 추가

--- feature/login/build.gradle.kts
+++ feature/login/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     defaultConfig {
         minSdk = 24
 
-        buildConfigField("String", "WEB_CLIENT_ID", properties.getProperty("web_client_id") ?: "")
+        buildConfigField("String", "WEB_CLIENT_ID", properties.getProperty("web_client_id") ?: "web_client_id is empty")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
