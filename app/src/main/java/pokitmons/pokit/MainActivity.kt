7149c80a5dcddc81bbba52a15b5c9e976774466c
jiwon2724 <stopone3119@naver.com>
2024-07-15 15:01:01 +0900

    [feature] LoginNavHost 구현

--- app/src/main/java/pokitmons/pokit/MainActivity.kt
+++ app/src/main/java/pokitmons/pokit/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import pokitmons.pokit.core.ui.theme.PokitTheme
-import pokitmons.pokit.login.LoginScreen
+import pokitmons.pokit.keyword.KeywordScreen
+import pokitmons.pokit.navigation.LoginNavHost
+import pokitmons.pokit.nickname.InputNicknameScreen
+import pokitmons.pokit.success.SignUpSuccessScreen
+import pokitmons.pokit.terms.TermsOfServiceScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PokitTheme {
-                LoginScreen()
+                LoginNavHost()
             }
         }
     }
