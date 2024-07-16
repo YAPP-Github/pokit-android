2c5d4903d0437eba70656c8391e584dbcd53396a
yunsehwan <sh10302@naver.com>
2024-07-16 18:35:04 +0900

    [CHORE] #14 ModifyBottomSheet, RemoveItemBottomSheet를 PokitBottomSheet 를 포함하지 않는 방식으로 수정

---
+++ core/ui/src/main/java/pokitmons/pokit/core/ui/components/template/modifybottomsheet/ModifyBottomSheetContent.kt
@@ -0,0 +1,45 @@
+package pokitmons.pokit.core.ui.components.template.modifybottomsheet
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import pokitmons.pokit.core.ui.R
+import pokitmons.pokit.core.ui.components.template.modifybottomsheet.subcomponents.ModifyBottomSheetItem
+
+@Composable
+fun ModifyBottomSheetContent(
+    onClickShare: (() -> Unit)? = null,
+    onClickModify: (() -> Unit)? = null,
+    onClickRemove: (() -> Unit)? = null,
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        onClickShare?.let { onClickShare ->
+            ModifyBottomSheetItem(
+                onClick = {},
+                title = stringResource(id = R.string.share),
+                painter = painterResource(id = R.drawable.icon_24_share)
+            )
+        }
+
+        onClickModify?.let { onClickModify ->
+            ModifyBottomSheetItem(
+                onClick = onClickModify,
+                title = stringResource(id = R.string.modify),
+                painter = painterResource(id = R.drawable.icon_24_edit)
+            )
+        }
+
+        onClickRemove?.let { onClickRemove ->
+            ModifyBottomSheetItem(
+                onClick = onClickRemove,
+                title = stringResource(id = R.string.remove),
+                painter = painterResource(id = R.drawable.icon_24_trash)
+            )
+        }
+    }
+}
--- core/ui/src/main/java/pokitmons/pokit/core/ui/components/template/modifybottomsheet/Preview.kt
+++ core/ui/src/main/java/pokitmons/pokit/core/ui/components/template/modifybottomsheet/Preview.kt
@@ -1,37 +1,22 @@
 package pokitmons.pokit.core.ui.components.template.modifybottomsheet
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import pokitmons.pokit.core.ui.theme.PokitTheme
 
 @Preview(showBackground = true)
 @Composable
-private fun ModifyBottomSheetPreview() {
-    var showBottomSheet by remember { mutableStateOf(false) }
-
+private fun ModifyBottomSheetContentPreview() {
     PokitTheme {
         Surface(modifier = Modifier.fillMaxSize()) {
-            Button(onClick = { showBottomSheet = true }) {
-                Text(text = "Click!")
-            }
-
-            if (showBottomSheet) {
-                ModifyBottomSheet(
-                    onHideBottomSheet = {},
-                    onClickRemove = {},
-                    onClickModify = {},
-                    onClickShare = {}
-                )
-            }
+            ModifyBottomSheetContent(
+                onClickRemove = {},
+                onClickModify = {},
+                onClickShare = {}
+            )
         }
     }
 }
