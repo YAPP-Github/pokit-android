4f29c30d8886f6e18508e1fad27b6cf5a0ca20dc
jiwon2724 <stopone3119@naver.com>
2024-07-16 21:43:25 +0900

    [chore] 컨벤션 반영

--- core/ui/src/main/java/pokitmons/pokit/core/ui/components/block/pokitlist/PokitList.kt
+++ core/ui/src/main/java/pokitmons/pokit/core/ui/components/block/pokitlist/PokitList.kt
@@ -6,17 +6,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import pokitmons.pokit.core.ui.R
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import pokitmons.pokit.core.ui.components.block.pokitlist.attributes.PokitListState
@@ -27,9 +22,6 @@ fun <T> PokitList(
     item: T,
     title: String,
     sub: String,
-    onClickKebab: (T) -> Unit,
-    modifier: Modifier = Modifier,
-    state: PokitListState = PokitListState.DISABLE,
     onClickItem: (T) -> Unit,
     modifier: Modifier = Modifier,
     state: PokitListState = PokitListState.DEFAULT,
@@ -62,20 +54,6 @@ fun <T> PokitList(
             Text(
                 text = sub,
                 style = PokitTheme.typography.detail1.copy(color = subTextColor)
-            )
-        }
-
-        IconButton(
-            onClick = { onClickKebab(item) },
-            modifier = Modifier
-                .padding(0.dp)
-                .align(Alignment.Top),
-            enabled = state != PokitListState.DISABLE
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.icon_24_kebab),
-                contentDescription = null,
-                modifier = Modifier.size(24.dp)
             )
         }
     }
