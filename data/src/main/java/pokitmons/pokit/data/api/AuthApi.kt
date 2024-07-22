a0497b022820d3df093353a9ed137b72dff0a489
jiwon2724 <stopone3119@naver.com>
2024-07-22 15:52:56 +0900

    [chore] auth api 수정

--- data/src/main/java/pokitmons/pokit/data/api/AuthApi.kt
+++ data/src/main/java/pokitmons/pokit/data/api/AuthApi.kt
@@ -2,6 +2,7 @@ package pokitmons.pokit.data.api
 
 import pokitmons.pokit.data.model.auth.request.SNSLoginRequest
 import pokitmons.pokit.data.model.auth.request.SignUpRequest
+import pokitmons.pokit.data.model.auth.response.DuplicateNicknameResponse
 import pokitmons.pokit.data.model.auth.response.SNSLoginResponse
 import pokitmons.pokit.data.model.auth.response.SignUpResponse
 import pokitmons.pokit.data.model.common.PokitResponse
@@ -24,5 +25,5 @@ interface AuthApi {
     @GET("user/duplicate/{nickname}")
     suspend fun checkDuplicateNickname(
         @Path("nickname") nickname: String
-    ): PokitResponse<SNSLoginResponse>
+    ): PokitResponse<DuplicateNicknameResponse>
 }
