7b42aa32ce1d640988acccc574f60767797f2b2f
jiwon2724 <stopone3119@naver.com>
2024-07-26 14:35:26 +0900

    [chore] @AndroidEntryPoint 애노테이션 추가

--- app/src/main/java/pokitmons/pokit/MainActivity.kt
+++ app/src/main/java/pokitmons/pokit/MainActivity.kt
@@ -3,9 +3,11 @@ package pokitmons.pokit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 import pokitmons.pokit.core.ui.theme.PokitTheme
 import pokitmons.pokit.navigation.LoginNavHost
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
