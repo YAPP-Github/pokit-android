08e9e01dcebf3ede7823fa341db50d98abfdf1c2
jiwon2724 <stopone3119@naver.com>
2024-07-29 18:22:38 +0900

    [chore] 중복 닉네임 api 세팅

--- data/src/main/java/pokitmons/pokit/data/api/AuthApi.kt
+++ data/src/main/java/pokitmons/pokit/data/api/AuthApi.kt
@@ -1,13 +1,21 @@
 package pokitmons.pokit.data.api
 
 import pokitmons.pokit.data.model.auth.request.SNSLoginRequest
+import pokitmons.pokit.data.model.auth.response.DuplicateNicknameResponse
 import pokitmons.pokit.data.model.auth.response.SNSLoginResponse
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface AuthApi {
     @POST("auth/signin")
     suspend fun snsLogin(
         @Body snsLoginRequest: SNSLoginRequest,
     ): SNSLoginResponse
+
+    @GET("user/duplicate/{nickname}")
+    suspend fun checkDuplicateNickname(
+        @Path(value = "nickname") nickname: String
+    ): DuplicateNicknameResponse
 }
