f5e8a84a927e59da293211ee9d83ee9b75ae1b89
jiwon2724 <stopone3119@naver.com>
2024-08-11 20:02:00 +0900

    [ui] Pokit 화면 구현

--- feature/home/src/main/java/pokitmons/pokit/home/pokit/PokitScreen.kt
+++ feature/home/src/main/java/pokitmons/pokit/home/pokit/PokitScreen.kt
@@ -1,18 +1,22 @@
 package pokitmons.pokit.home.pokit
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import pokitmons.pokit.core.ui.components.block.pokitcard.PokitCard
+import pokitmons.pokit.home.HomeMid
 
 @Composable
-fun PokitScreen() {
+fun PokitScreen(modifier: Modifier) {
     val dummy = arrayListOf<PokitCardDummy>().apply {
         add(PokitCardDummy())
         add(PokitCardDummy())
@@ -21,20 +25,28 @@ fun PokitScreen() {
         add(PokitCardDummy())
     }.toList()
 
-    LazyVerticalGrid(
-        modifier = Modifier.fillMaxSize(),
-        columns = GridCells.Fixed(2),
-        verticalArrangement = Arrangement.spacedBy(12.dp),
-        horizontalArrangement = Arrangement.spacedBy(12.dp),
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 20.dp)
+            .background(color = Color.White)
+            .fillMaxSize()
     ) {
-        items(dummy) { pokitInfo ->
-            PokitCard(
-                text = pokitInfo.text,
-                linkCount = pokitInfo.linkCount,
-                painter = null,
-                onClick = { /*TODO*/ },
-                onClickKebab = { /*TODO*/ }
-            )
+        HomeMid()
+        LazyVerticalGrid(
+            modifier = Modifier.fillMaxSize(),
+            columns = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+        ) {
+            items(dummy) { pokitInfo ->
+                PokitCard(
+                    text = pokitInfo.text,
+                    linkCount = pokitInfo.linkCount,
+                    painter = null,
+                    onClick = { /*TODO*/ },
+                    onClickKebab = { /*TODO*/ }
+                )
+            }
         }
     }
 }
@@ -43,9 +55,3 @@ data class PokitCardDummy(
     val text: String = "요리/레시피",
     val linkCount: Int = 10,
 )
-
-@Preview
-@Composable
-fun PokitCardPreview() {
-    PokitScreen()
-}
