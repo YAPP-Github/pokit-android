496e61dfe90084d9f26f53985c551406aed4b948
jiwon2724 <stopone3119@naver.com>
2024-08-18 17:50:29 +0900

    [feature] 스플래쉬 화면 구현

--- app/src/main/java/pokitmons/pokit/MainActivity.kt
+++ app/src/main/java/pokitmons/pokit/MainActivity.kt
@@ -3,7 +3,20 @@ package pokitmons.pokit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 import pokitmons.pokit.core.ui.theme.PokitTheme
 import pokitmons.pokit.navigation.LoginNavHost
 
@@ -12,9 +25,32 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var showSplash by remember { mutableStateOf(true) }
+
+            LaunchedEffect(Unit) {
+                delay(1500)
+                showSplash = false
+            }
+
             PokitTheme {
-                LoginNavHost()
+                if (showSplash) {
+                    SplashScreen()
+                } else {
+                    LoginNavHost()
+                }
             }
         }
+    }
+}
+
+@Composable
+fun SplashScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = pokitmons.pokit.core.ui.R.drawable.logo_white),
+            contentDescription = null)
     }
 }
