0c2878e382d5d60a865cee7b7a391bc68834f8ed
yunsehwan <sh10302@naver.com>
2024-08-23 21:42:29 +0900

    [UI] #41 status bar 색상을 흰색으로 수정

--- core/ui/src/main/java/pokitmons/pokit/core/ui/theme/Theme.kt
+++ core/ui/src/main/java/pokitmons/pokit/core/ui/theme/Theme.kt
@@ -1,9 +1,14 @@
 package pokitmons.pokit.core.ui.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 import pokitmons.pokit.core.ui.theme.color.LocalPokitColorDarkScheme
 import pokitmons.pokit.core.ui.theme.color.LocalPokitColorScheme
 import pokitmons.pokit.core.ui.theme.color.PokitColors
@@ -29,6 +34,16 @@ fun PokitTheme(
     val colorScheme = when {
         darkTheme -> LocalPokitColorDarkScheme.current
         else -> LocalPokitColorScheme.current
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.backgroundBase.toArgb()
+            // dark theme 지원시 true 대신 !darkTheme를 할당하도록 수정 예정
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
+        }
     }
 
     CompositionLocalProvider(
