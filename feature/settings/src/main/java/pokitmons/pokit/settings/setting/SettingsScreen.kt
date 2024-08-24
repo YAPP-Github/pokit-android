b9048e1c8fcd6495c59c69ba5cc0d205e824db69
jiwon2724 <stopone3119@naver.com>
2024-08-25 04:12:16 +0900

    [feature] 공지사항 등 url 링크 이동

--- feature/settings/src/main/java/pokitmons/pokit/settings/setting/SettingsScreen.kt
+++ feature/settings/src/main/java/pokitmons/pokit/settings/setting/SettingsScreen.kt
@@ -1,11 +1,15 @@
 package pokitmons.pokit.settings.setting
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import pokitmons.pokit.core.ui.components.template.bottomsheet.PokitBottomSheet
@@ -18,6 +22,10 @@ fun SettingsScreen(
     settingViewModel: SettingViewModel,
     onNavigateToEditNickname: () -> Unit,
 ) {
+//    settingViewModel.changeBottomSheetHideState()
+
+    val context = LocalContext.current
+
     Column(modifier = Modifier.fillMaxWidth()) {
         SettingHeader()
         Spacer(modifier = Modifier.height(16.dp))
@@ -26,17 +34,22 @@ fun SettingsScreen(
                 onNavigateToEditNickname()
             }
             SettingItem(title = stringResource(StringResource.notification_settings)) {
+                // TODO 커스텀 토스트 메세지
             }
 
             DividerItem()
 
             SettingItem(title = stringResource(StringResource.announcements)) {
+                moveToUrl(url = "https://www.notion.so/POKIT-d97c81534b354cfebe677fbf1fbfe2b2", context)
             }
             SettingItem(title = stringResource(StringResource.terms_of_service)) {
+                moveToUrl(url = "https://www.notion.so/3bddcd6fd00043abae6b92a50c39b132?pvs=4", context)
             }
             SettingItem(title = stringResource(StringResource.privacy_policy)) {
+                moveToUrl(url = "https://www.notion.so/de3468b3be1744538c22a333ae1d0ec8", context)
             }
             SettingItem(title = stringResource(StringResource.customer_support)) {
+                moveToUrl(url = "https://www.instagram.com/pokit.official/", context)
             }
 
             DividerItem()
@@ -56,10 +69,16 @@ fun SettingsScreen(
         show = settingViewModel.isBottomSheetVisible
     ) {
         TwoButtonBottomSheetContent(
-            title = stringResource(id = StringResource.logout_title),
-            rightButtonText = stringResource(id = StringResource.logout),
+            subText = stringResource(id = StringResource.delete_account_sub),
+            title = stringResource(id = StringResource.delete_account_title),
+            rightButtonText = stringResource(id = StringResource.start_delete_account),
             onClickLeftButton = { settingViewModel.changeBottomSheetHideState() },
             onClickRightButton = {}
         )
     }
+}
+
+fun moveToUrl(url: String, context: Context) {
+    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+    context.startActivity(intent)
 }
