2895413ad3afaedecb423cbb691b3435f9aad778
yunsehwan <sh10302@naver.com>
2024-09-06 15:34:15 +0900

    [CHORE] #69-modifier에서 height, width가 동일한 경우 size를 사용하도록 수정

--- core/ui/src/main/java/pokitmons/pokit/core/ui/components/template/pookiempty/EmptyPooki.kt
+++ core/ui/src/main/java/pokitmons/pokit/core/ui/components/template/pookiempty/EmptyPooki.kt
@@ -5,7 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -30,8 +30,7 @@ fun EmptyPooki(
         ) {
             Image(
                 modifier = Modifier
-                    .height(180.dp)
-                    .width(180.dp),
+                    .size(180.dp),
                 painter = painterResource(id = R.drawable.empty_pooki),
                 contentDescription = "empty"
             )
--- core/ui/src/main/java/pokitmons/pokit/core/ui/components/template/pookierror/ErrorPokki.kt
+++ core/ui/src/main/java/pokitmons/pokit/core/ui/components/template/pookierror/ErrorPokki.kt
@@ -5,7 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -38,8 +38,7 @@ fun ErrorPooki(
         ) {
             Image(
                 modifier = Modifier
-                    .height(pookiSize)
-                    .width(pookiSize),
+                    .size(pookiSize),
                 painter = painterResource(id = R.drawable.cry_pooki),
                 contentDescription = "empty"
             )
