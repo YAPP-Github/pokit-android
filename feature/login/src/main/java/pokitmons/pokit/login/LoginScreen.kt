c59582fef38450f8ded73bfed98238c62362f080
yunsehwan <sh10302@naver.com>
2024-09-28 18:38:17 +0900

    [FIX] 로그인 화면에서 자동로그인 이벤트 발생시 화면 이동 이벤트가 2번 수행되는 문제 수정

--- feature/login/src/main/java/pokitmons/pokit/login/LoginScreen.kt
+++ feature/login/src/main/java/pokitmons/pokit/login/LoginScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
@@ -47,19 +48,22 @@ fun LoginScreen(
     val coroutineScope = rememberCoroutineScope()
 
     // TODO 리팩토링
-    when (loginState) {
-        is LoginState.Init -> Unit
-        is LoginState.Login -> {
-            loginViewModel.changeState()
-            onNavigateToTermsOfServiceScreen()
+    LaunchedEffect(loginState) {
+        when (loginState) {
+            is LoginState.Init -> Unit
+            is LoginState.Login -> {
+                loginViewModel.changeState()
+                onNavigateToTermsOfServiceScreen()
+            }
+            is LoginState.Registered -> {
+                loginViewModel.changeState()
+                onNavigateToHomeScreen()
+            }
+            is LoginState.Failed -> loginViewModel.setVisible(true)
+            is LoginState.AutoLogin -> onNavigateToHomeScreen()
         }
-        is LoginState.Registered -> {
-            loginViewModel.changeState()
-            onNavigateToHomeScreen()
-        }
-        is LoginState.Failed -> loginViewModel.setVisible(true)
-        is LoginState.AutoLogin -> onNavigateToHomeScreen()
     }
+
 
     Box(
         modifier = Modifier
