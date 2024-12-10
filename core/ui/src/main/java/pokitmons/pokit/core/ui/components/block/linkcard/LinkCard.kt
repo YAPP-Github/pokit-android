d6fd05e6df3acd6b82ec876a78604f7dbba06dcd
yunsehwan <sh10302@naver.com>
2024-12-10 19:29:03 +0900

    [UI] #86 LinkCard 세부 디자인 수정

--- core/ui/src/main/java/pokitmons/pokit/core/ui/components/block/linkcard/LinkCard.kt
+++ core/ui/src/main/java/pokitmons/pokit/core/ui/components/block/linkcard/LinkCard.kt
@@ -2,7 +2,6 @@ package pokitmons.pokit.core.ui.components.block.linkcard
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -163,17 +162,28 @@ fun<T> LinkCard(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.spacedBy(6.dp)
                 ) {
+                    if (badgeText?.isNotBlank() == true) {
+                        Text(
+                            text = badgeText,
+                            modifier = Modifier
+                                .weight(1f, fill = false)
+                                .background(
+                                    color = PokitTheme.colors.backgroundPrimary,
+                                    shape = RoundedCornerShape(4.dp)
+                                )
+                                .padding(horizontal = 8.dp, vertical = 4.dp),
+                            style = PokitTheme.typography.label4.copy(color = PokitTheme.colors.textTertiary),
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+
                     if (notRead) {
                         Text(
                             text = stringResource(id = R.string.not_read),
                             modifier = Modifier
-                                .border(
-                                    width = 1.dp,
-                                    color = PokitTheme.colors.brand,
-                                    shape = RoundedCornerShape(4.dp)
-                                )
                                 .background(
-                                    color = PokitTheme.colors.backgroundBase,
+                                    color = Color(0xFFFFF3EA),
                                     shape = RoundedCornerShape(4.dp)
                                 )
                                 .padding(horizontal = 8.dp, vertical = 4.dp),
@@ -181,55 +191,44 @@ fun<T> LinkCard(
                         )
                     }
 
-                    if (badgeText?.isNotBlank() == true) {
+                    if (hasMemo) {
                         Box(
-                            Modifier.weight(1f)
+                            modifier = Modifier.size(20.dp)
                         ) {
-                            Text(
-                                text = badgeText,
+                            Icon(
                                 modifier = Modifier
+                                    .height(20.dp)
+                                    .aspectRatio(1f)
                                     .background(
                                         color = PokitTheme.colors.backgroundPrimary,
                                         shape = RoundedCornerShape(4.dp)
                                     )
-                                    .padding(horizontal = 8.dp, vertical = 4.dp),
-                                style = PokitTheme.typography.label4.copy(color = PokitTheme.colors.textTertiary),
-                                maxLines = 1,
-                                overflow = TextOverflow.Ellipsis
+                                    .padding(2.dp),
+                                painter = painterResource(id = R.drawable.icon_24_file),
+                                tint = PokitTheme.colors.iconSecondary,
+                                contentDescription = "hasMemo"
                             )
                         }
                     }
 
-                    if (hasMemo) {
-                        Icon(
-                            modifier = Modifier
-                                .height(20.dp)
-                                .aspectRatio(1f)
-                                .background(
-                                    color = PokitTheme.colors.backgroundPrimary,
-                                    shape = RoundedCornerShape(4.dp)
-                                )
-                                .padding(2.dp),
-                            painter = painterResource(id = R.drawable.icon_24_file),
-                            tint = PokitTheme.colors.iconSecondary,
-                            contentDescription = "hasMemo"
-                        )
-                    }
-
                     if (hasMember) {
-                        Icon(
-                            modifier = Modifier
-                                .height(20.dp)
-                                .aspectRatio(1f)
-                                .background(
-                                    color = PokitTheme.colors.backgroundPrimary,
-                                    shape = RoundedCornerShape(4.dp)
-                                )
-                                .padding(2.dp),
-                            painter = painterResource(id = R.drawable.icon_24_member),
-                            tint = PokitTheme.colors.iconSecondary,
-                            contentDescription = "hasMemo"
-                        )
+                        Box (
+                            modifier = Modifier.size(20.dp)
+                        ) {
+                            Icon(
+                                modifier = Modifier
+                                    .height(20.dp)
+                                    .aspectRatio(1f)
+                                    .background(
+                                        color = PokitTheme.colors.backgroundPrimary,
+                                        shape = RoundedCornerShape(4.dp)
+                                    )
+                                    .padding(2.dp),
+                                painter = painterResource(id = R.drawable.icon_24_member),
+                                tint = PokitTheme.colors.iconSecondary,
+                                contentDescription = "hasMemo"
+                            )
+                        }
                     }
                 }
             }
